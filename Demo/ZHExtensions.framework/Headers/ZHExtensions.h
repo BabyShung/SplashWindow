6588d6ffe0dcef2e8b19c6f4bf57a40fbc5d2e35
BabyShung <babyshung8@gmail.com>
2017-05-06 09:39:14 -0400

    Relink framework in Demo

---
+++ Demo/ZHExtensions.framework/Headers/ZHExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ZHExtensions.h
+//  ZHExtensions
+//
+//  Created by Hoa Zheng on 4/30/17.
+//  Copyright © 2017 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZHExtensions.
+FOUNDATION_EXPORT double ZHExtensionsVersionNumber;
+
+//! Project version string for ZHExtensions.
+FOUNDATION_EXPORT const unsigned char ZHExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZHExtensions/PublicHeader.h>
+
+
