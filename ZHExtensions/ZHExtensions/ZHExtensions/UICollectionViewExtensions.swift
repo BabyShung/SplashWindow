06e20c24115d4d449e596d9db38461c9284c0467
BabyShung <babyshung8@gmail.com>
2017-06-03 16:59:34 -0400

    update extensions

--- ZHExtensions/ZHExtensions/ZHExtensions/UICollectionViewExtensions.swift
+++ ZHExtensions/ZHExtensions/ZHExtensions/UICollectionViewExtensions.swift
@@ -3,6 +3,13 @@ import UIKit
 
 public extension UICollectionView {
     
+    public func dequeue<T: UICollectionViewCell>(_ indexPath: IndexPath) -> T where T: CollectionViewCellIdentifiable {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.cellIdentifier, for: indexPath) as? T else {
+            fatalError("Can't instantiate VC with identifier \(T.cellIdentifier) ")
+        }
+        return cell
+    }
+    
     public func register<T: UICollectionViewCell>(_: T.Type) {
         register(T.self, forCellWithReuseIdentifier: String(describing: T.self))
     }
@@ -16,3 +23,15 @@ public extension UICollectionView {
         return dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as! T
     }
 }
+
+public protocol CollectionViewCellIdentifiable {
+    static var cellIdentifier: String { get }
+}
+
+public extension CollectionViewCellIdentifiable where Self: UICollectionViewCell {
+    public static var cellIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell : CollectionViewCellIdentifiable { }
--- ZHExtensions/ZHExtensions/ZHExtensions/UIStoryboardExtension.swift
+++ ZHExtensions/ZHExtensions/ZHExtensions/UIStoryboardExtension.swift
@@ -2,6 +2,29 @@
 import UIKit
 
 public extension UIStoryboard {
+    
+    public func instantiateViewController<T: UIViewController>() -> T where T: StoryboardIdentifiable {
+        guard let viewController = self.instantiateViewController(withIdentifier: T.storyboardIdentifier) as? T else {
+            fatalError("Can't instantiate VC with identifier \(T.storyboardIdentifier) ")
+        }
+        
+        return viewController
+    }
+}
+
+public protocol StoryboardIdentifiable {
+    static var storyboardIdentifier: String { get }
+}
+
+public extension StoryboardIdentifiable where Self: UIViewController {
+    public static var storyboardIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UIViewController : StoryboardIdentifiable { }
+
+public extension UIStoryboard {
 
     class func named(_ storyboardName: String, vc: String) -> UIViewController {
         let sb = UIStoryboard.init(name: storyboardName, bundle: nil)
